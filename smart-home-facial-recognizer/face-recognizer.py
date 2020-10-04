3f5474b9d96b01b1e09be69a444541bbf37d5c0f
FixxxarN <tornstromandre@gmail.com>
2020-10-04 22:25:52 +0200

    Made the facial recognition better by giving it 2 more images of André and also made it possible to send the person to the backend whenever there is a new person standing in front of the mirror

--- smart-home-facial-recognizer/face-recognizer.py
+++ smart-home-facial-recognizer/face-recognizer.py
@@ -1,5 +1,6 @@
 #import time will be used later when we give the camera some seconds to warm up.
 import time
+import requests
 
 #cv2 is the module that will do all the training and face recognition.
 import cv2
@@ -34,6 +35,9 @@ vs = VideoStream(src=0, usePiCamera=usingPiCamera, resolution=frameSize, framera
 #Let's the camera warm up in 2 seconds
 time.sleep(2.0)
 
+whoIsStandingInFrontOfMirror = []
+whoHasBeenSentToBackend = ""
+
 while True:
     frame = vs.read()
     
@@ -48,12 +52,23 @@ while True:
         roi_gray = gray[y:y+h, x:x+w]
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 0), 2)
         id_, conf = recognizer.predict(roi_gray)
-        if conf >= 80:
-            font = cv2.FONT_HERSHEY_SIMPLEX
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        color = (255, 255, 255)
+        stroke = 2
+        if conf >= 85:
             name = names[id_]
-            color = (255, 255, 255)
-            stroke = 2
+            whoIsStandingInFrontOfMirror.append(name)
+            
+            if whoIsStandingInFrontOfMirror.count(max(whoIsStandingInFrontOfMirror, key=whoIsStandingInFrontOfMirror.count)) > 30:
+                if max(whoIsStandingInFrontOfMirror, key=whoIsStandingInFrontOfMirror.count) != whoHasBeenSentToBackend:
+                    url = "http://192.168.1.33:9999/smart-home-backend/weatherforecast?text=" + max(whoIsStandingInFrontOfMirror, key=whoIsStandingInFrontOfMirror.count)
+                    r = requests.post(url)
+                    print(r.text)
+                    whoHasBeenSentToBackend = max(whoIsStandingInFrontOfMirror, key=whoIsStandingInFrontOfMirror.count)
+                whoIsStandingInFrontOfMirror = []
             cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+        else:
+           cv2.putText(frame, "Unknown", (x,y), font, 1, color, stroke, cv2.LINE_AA) 
             
     cv2.imshow('Face-recognizer', frame)
     key = cv2.waitKey(1) & 0xFF
